d07ef0d8d0458f14f851e9134177052137d7979a
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2023-07-04 10:34:56 +0530

    Basic playaround with data types.

---
+++ 2.1 Data Types.py
@@ -0,0 +1,16 @@
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+# Warning. Do not change the code on lines 1-3. Your program should work for different inputs. e.g. any two-digit number.
+
+#Answer
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# str_two_digit_num = str(two_digit_number) -> not required as all the inputted strings are strings only
+num1 = int(str_two_digit_num[0])
+num2 = int(str_two_digit_num[1])
+print(num1 + num2)
