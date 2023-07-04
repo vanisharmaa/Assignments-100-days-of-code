dc4d129c28910577b6e2d25b8d27887224457e38
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2023-07-04 13:25:09 +0530

    String.split() and random.randint() functions

---
+++ 4.2 Banker Roulette.py
@@ -0,0 +1,18 @@
+# You are going to write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+
+# Important: You are not allowed to use the choice() function.
+
+# Answer
+
+# Import the random module here
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+length = len(names);
+random_index = random.randint(0, length-1);
+print(names[random_index] + " is going to buy the meal today!")
