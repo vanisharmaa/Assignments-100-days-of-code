3f97514b5933e0bb861b42c44cc30e3630edd08c
vanisharmaa <109752227+vanisharmaa@users.noreply.github.com>
2023-07-08 21:56:21 +0530

    While loop

---
+++ 6.1 The Hurdles Loop Challenge.py
@@ -0,0 +1,18 @@
+# Program for Reeborg's world to make a square
+
+def turn_right() :
+    turn_left()
+    turn_left()
+    turn_left()
+
+def make_square() :
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()    
+    
+make_square()
